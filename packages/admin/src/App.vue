ea7dc331e5e249159f124e6755d0c4b760d54600
midsummer <h0109l@outlook.com>
2023-08-03 12:42:34 +0800

    ✨ feat: login页

--- packages/admin/src/App.vue
+++ packages/admin/src/App.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import {useUserStore} from "@/stores/user";
 
-const user = useUserStore()
-
-const num = ref(0)
-const add = () => {
-  num.value++
-}
 </script>
 
 <template>
