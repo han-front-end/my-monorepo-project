4c70a4bfb820173006828b993b621ab57361d47b
midsummer <h0109l@outlook.com>
2023-08-26 20:58:13 +0800

    🗑️ build: 移除打包文件 git忽略cli/build

--- packages/midsummer-ui/cli/build/template/style.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//创建样式文件的模板
-function genStyleTemplate(name) {
-    return ".m-".concat(name, " {\n  /* your component style */\n  \n}\n");
-}
-exports.default = genStyleTemplate;
--- packages/midsummer-ui/cli/build/template/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.upperFirst = void 0;
-function upperFirst(str) {
-    return str[0].toUpperCase() + str.slice(1);
-}
-exports.upperFirst = upperFirst;
