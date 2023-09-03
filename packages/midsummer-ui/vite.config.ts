24d2807cbc472b1a9cd0be54134d621e57ccb23d
midsummer <h0109l@outlook.com>
2023-09-03 18:21:02 +0800

    🐛 fix: 修复遗留的ts报错

--- packages/midsummer-ui/vite.config.ts
+++ packages/midsummer-ui/vite.config.ts
@@ -1,20 +1,24 @@
 import {defineConfig} from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from "path";
+import * as path from "path";
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src'),
+      "build": path.resolve(__dirname, 'build')
     }
   },
   plugins: [
     vue({
       include: [/\.vue$/]
     }),
-    vueJsx({})
+    vueJsx({}),
   ],
+  build: {
+    cssCodeSplit: true,
+  },
   test: {
     globals: true,
     environment: "happy-dom",
